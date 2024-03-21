03aee3c4f8188618f6d12a9ebd76e7c26aa0d404
Николай Моденов <uptb.modenov@yandex.ru>
2024-03-21 16:11:15 +0300

    send message

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,6 +1,10 @@
-from django.contrib import admin
-from django.urls import include, path
-from django.views.generic import TemplateView
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from users.views import SignUpView
 
 urlpatterns = [
+    path('v1/auth/signup/', SignUpView.as_view(), name='signup'),
+    path('v1/auth/token/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
 ]
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -98,7 +98,7 @@ AUTH_USER_MODEL = 'users.YamdbUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
@@ -112,3 +112,7 @@ SIMPLE_JWT = {
 #    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# Указываем директорию, в которую будут сохраняться файлы писем:
+EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
