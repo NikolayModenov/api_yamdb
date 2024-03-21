8b8d1b761c06870987f49ee3ca094655220724cd
Николай Моденов <uptb.modenov@yandex.ru>
2024-03-21 18:37:05 +0300

    add email

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -10,5 +10,12 @@ ROLES = (
 
 
 class YamdbUser(AbstractUser):
+    email = models.EmailField(
+        verbose_name='адрес электронной почты',
+        max_length=254,
+        null=False,
+        unique=True,
+        blank=False,
+    )
     bio = models.TextField('Биография', blank=True)
     role = models.CharField(default="user", choices=ROLES, max_length=9)
