ec21ee02c2fbd84171a8764e17f976d21d501447
Николай Моденов <uptb.modenov@yandex.ru>
2024-03-24 01:25:54 +0300

    fix permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -9,7 +9,7 @@ class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             not request.user.is_authenticated
-            or request.user.role == 'admin' or request.user.is_staff
+            or request.user.is_admin or request.user.is_staff
         )
 
 
@@ -19,7 +19,7 @@ class AdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             (request.method in permissions.SAFE_METHODS)
-            or (request.user.is_authenticated and request.user.role == "admin")
+            or (request.user.is_authenticated and request.user.is_admin)
         )
 
 
